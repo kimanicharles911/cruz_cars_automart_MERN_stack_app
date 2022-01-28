cecb38ed86192dd6b455cc73888abc47b0b50d14
kimanicharles911 <kimanicharles911@gmail.com>
2022-01-28 13:12:26 +0300

    add validation to schema

--- models/CarModel.js
+++ models/CarModel.js
@@ -4,35 +4,50 @@ import mongoose from 'mongoose';
 const carSchema = new mongoose.Schema({
   modelSpec: {
     type: Number,
+    min: 1700,
+    max: 2122,
     required: true
   },
   makeSpec: {
     type: String,
+    minLength: 3,
+    maxLength: 100,
     required: true
   },
   sellingPrice: {
     type: Number,
+    min: 1,
+    max: 100000000,
     required: true
   },
   mileage: {
     type: Number,
+    min: 0,
+    max: 100000000,
     required: true
   },
   photo: {
     type: String,
+    maxLength: 100,
     required: true
   },
   registrationNumber: {
     type: String,
+    minLength: 6,
+    maxLength: 23,
     required: true,
     unique: true
   },
   owner: {
     type: String,
+    minLength: 3,
+    maxLength: 30,
     required: true
   },
   address: {
     type: String,
+    minLength: 4,
+    maxLength: 100,
     required: true
   }
 }, {
